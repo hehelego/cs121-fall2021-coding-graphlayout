38d6aa17065b6bfe291bcb88da812379b8705918
hehelego <2364261262@qq.com>
2022-01-06 21:03:49 +0800

    upd fix test script

--- once.fish
+++ once.fish
@@ -7,6 +7,6 @@ set raw_size (python src/graph_size.py $in_edge)
 set N (echo $raw_size | cut -d' ' -f1)
 set M (echo $raw_size | cut -d' ' -f2)
 
-./bin/cpu $graph_size $iter $in_edge $out_coor
+./bin/cpu $N $M $iter $in_edge $out_coor
 
 python src/visualize.py $in_edge $out_coor $out_img
