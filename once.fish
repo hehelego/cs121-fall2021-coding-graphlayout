2831e4eaa3e20f1fbee7bfbfc6a4a13830cfaa94
hehelego <2364261262@qq.com>
2022-01-06 20:59:14 +0800

    upd fix test script

--- once.fish
+++ once.fish
@@ -1,9 +1,12 @@
 #!/usr/bin/fish
-set in_edge data/wiki-vote.txt
-set out_coor data/wiki-vote.coordinate
-set out_img data/wiki-vote.png
+set in_edge data/wikivote.in
+set out_coor data/wikivote.coordinate
+set out_img data/wikivote.png
 set iter 100
-set graph_size (python src/graph_size.py $in_edge)
+set raw_size (python src/graph_size.py $in_edge)
+set N (echo $raw_size | cut -d' ' -f1)
+set M (echo $raw_size | cut -d' ' -f2)
 
-bin/cpu $graph_size $iter $in_edge $out_coor
+./bin/cpu $graph_size $iter $in_edge $out_coor
+
 python src/visualize.py $in_edge $out_coor $out_img
